9c519fccb3d616e77865adc09ce65e06a9448351
jkowalska <jkowalska@sigma.ug.edu.pl>
2013-10-27 13:03:14 +0100

    Update ex1.c

---
+++ ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* komentarz, który może się znajdować
+  w kilku liniach */
+  
+int main() {
+    int dystans = 100;    // deklaracja zmiennej
+    
+    printf("Przebyłeś %d mil.\n", dystans);
+
+    return 0;
+}
