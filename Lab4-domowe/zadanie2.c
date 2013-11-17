cc4adcb8ebd886d929ab90ff67ccbea8c4be08ab
jkowalska <jkowalska@sigma.ug.edu.pl>
2013-11-17 11:07:32 +0100

    Update zadanie2.c

--- Lab4-domowe/zadanie2.c
+++ Lab4-domowe/zadanie2.c
@@ -1,13 +1,20 @@
-/* Program prosi o podanie kodu ASCII, a następnie wypisuje znak odpowiadający temu kodowi */
+/* Program prosi o podanie kodu ASCII, a następnie wypisuje znak 
+odpowiadający temu kodowi */
 
 #include <stdio.h>
+#include <ctype.h>
 
-int main() { 
-	int kod;
-	
-	printf("Podaj kod ASCII: "); 
-	scanf("%d", &kod); 
-	printf("%c\n", kod); 
+int main() {
+  int kod;
+        
+  printf("Podaj kod ASCII: ");
+  scanf("%i", &kod);
+    
+  if(isprint(kod)) {
+    printf("%c\n", kod);
+  } else {
+    printf("Znak jest niedrukowalny\n");
+  }
 
-	return 0; 
-} 
+  return 0;
+}
