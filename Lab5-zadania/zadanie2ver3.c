e137701dd99f1fd7b8defdbca74f388b0b331866
jkowalska <jkowalska@sigma.ug.edu.pl>
2013-11-24 10:20:08 +0100

    zadanie2ver3.c

---
+++ Lab5-zadania/zadanie2ver3.c
@@ -0,0 +1,38 @@
+/* Napisz program wyświetlający poniższą szachownicę:
+
+* * * * * * * *
+ * * * * * * * *
+* * * * * * * *
+ * * * * * * * *
+* * * * * * * *
+ * * * * * * * *
+* * * * * * * *
+ * * * * * * * *
+*/
+
+#include <stdio.h>
+
+void rysuj_gwiazdki(char element[]); //deklarujemy element
+
+int main()
+{
+  int w = 0; 
+   
+  while (w < 5) 
+  {
+    rysuj_gwiazdki("* ");
+    rysuj_gwiazdki(" *");
+    ++w;
+  }
+  return 0;
+}
+
+void rysuj_gwiazdki(char element[])
+{
+int i = 0;
+  while (i < 10) {
+    printf("%s", element);
+    ++i;
+  }
+  printf("\n");
+}
