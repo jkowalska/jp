a2c735ba5f8510603dbc997c2cecdca65fc34d8d
jkowalska <jkowalska@sigma.ug.edu.pl>
2013-11-24 09:47:04 +0100

    Update zadanie2.c

--- Lab5-zadania/zadanie2.c
+++ Lab5-zadania/zadanie2.c
@@ -1,36 +1,38 @@
 /* Napisz program wyświetlający poniższą szachownicę:
 
 * * * * * * * *
- * * * * * * * *
 * * * * * * * *
- * * * * * * * *
 * * * * * * * *
- * * * * * * * *
 * * * * * * * *
- * * * * * * * *
+* * * * * * * *
+* * * * * * * *
+* * * * * * * *
+* * * * * * * *
 */
 
 #include <stdio.h>
 
 int main()
 {
-  int i, w, z;
-  
-  for() 
-  {
-    printf("* ");
-    //printf("* * * * * * * * ");
-  }
-    
-  printf("\n");
-    
-  for() 
-  {
-    printf(" *");
-    //printf(" * * * * * * * *");
-  } 
-    
-  printf("\n");
+  int i = 0, w = 0; //liczniki pętli
+   
+  while (w < 5) {
 
+    while (i < 10) {
+      printf("* ");
+      ++i;
+    }
+    printf("\n");
+    i = 0;
+    while (i < 10) {
+      printf(" *");
+      ++i;
+    }
+    i = 0;
+    printf("\n");
+
+    ++w;
+  }
   return 0;
+
 }
