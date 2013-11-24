ed7301a406a6bc18b769b189d513d10026fa3860
jkowalska <jkowalska@sigma.ug.edu.pl>
2013-11-24 09:43:47 +0100

    Update zadanie1.c

--- Lab5-zadania/zadanie1.c
+++ Lab5-zadania/zadanie1.c
@@ -1,16 +1,19 @@
-/* Napisz program, który wypisuje kolejne potęgi liczby 2 
+/* Napisz program, który wypisuje kolejne potęgi liczby 2
 nie przekraczające 2010 */
 
 #include <stdio.h>
-#include <math.h>
-
 int main()
 {
-  int n, x;
+  int wynik;
 
-  for (n = 0; n <= 2010; n++);
+  //liczba = 1;
+  wynik = 1;
+
+  while (wynik <= 2010) 
   {
-  printf("%d", power( n, 2));
+  	printf("%d\n", wynik);
+    //liczba = liczba * 2;
+    wynik = wynik * 2; //lub wynik *= 2;
   }
 
   return 0;
