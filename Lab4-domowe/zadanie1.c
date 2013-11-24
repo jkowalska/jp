5e1ec1b1e3afd2a298bbef2b293b005e6edc53b0
jkowalska <jkowalska@sigma.ug.edu.pl>
2013-11-24 01:51:24 +0100

    Update zadanie1.c

--- Lab4-domowe/zadanie1.c
+++ Lab4-domowe/zadanie1.c
@@ -1,1 +1,21 @@
-/* Zbadaj jak zachowuje się Sigma w sytuacji przepełnienia zmiennej całkowitej, przepełnienia zmiennej zmiennoprzecinkowej i niedomiaru zmiennej zmiennoprzecinkowej. Zastosuj metodę doświadczalną, czyli napisz programy, w których wystąpią te przepełnienia i niedomiary. Wskazówka: zajrzyj do plików limits.h oraz float.h w których zdefiniowano stałe symboliczne dla wszystkich rozmiarów danych oraz dla innych właściwości komputera i kompilatora. (Są też strony manuala limits.h i float.h.) */
+/* Zbadaj jak zachowuje się Sigma w sytuacji przepełnienia zmiennej całkowitej, 
+przepełnienia zmiennej zmiennoprzecinkowej i niedomiaru zmiennej zmiennoprzecinkowej. 
+Zastosuj metodę doświadczalną, czyli napisz programy, w których wystąpią te przepełnienia 
+i niedomiary. Wskazówka: zajrzyj do plików limits.h oraz float.h w których zdefiniowano 
+stałe symboliczne dla wszystkich rozmiarów danych oraz dla innych właściwości komputera 
+i kompilatora. (Są też strony manuala limits.h i float.h.) */
+
+#include <stdio.h>
+int main()
+{
+  int i = 2147483647;
+  unsigned int j = 4294967295;
+  printf("%d %d %d\n", i, i+1, i+2);
+  printf("%u %u %u\n", j, j+1, j+2);
+  return 0;
+}
+
+/* zmienna j po osiągnięciu wartości max rozpoczyna liczenie od początku, podobnie zmienna i, 
+ale podczas gdy zmienna j typu unsigned int rozpoczyna liczenie od zera, zmienna i typu int
+zaczyna je od wartości -2 147 483 647.*/
+  
