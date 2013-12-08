7467e5fc1b44adbbded07406b1bbabf97b5b34ad
jkowalska <jkowalska@sigma.ug.edu.pl>
2013-12-08 11:47:51 +0100

    dodano zadania

---
+++ Lab6-zadania/zadanie4ver3.c
@@ -0,0 +1,35 @@
+/* Napisz funkcję, która po wczytaniu liczby całkowitej wypisze jej cyfry
+zaczynając od ostatniej i kończąc na pierwszej. Na przykład po wczytaniu
+liczby '1410' funkcja powinna wypisać '0141'. */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char *inverted(int x);
+
+int main() {
+  int a;
+
+  printf("Wprowadź liczbę: ");
+  scanf("%d", &a);
+  printf("Cyfry od końca: %s\n", inverted(a));
+  printf("\n");
+
+  return 0;
+}
+
+char *inverted(int x) {
+
+  char *znaki = malloc(10);
+  i = 0;
+
+  while (x > 0) {
+    znaki[i] = '0' + x % 10;
+    x = x / 10;
+    i++;
+  }
+ 
+  znaki[i] = '\0';
+
+  return znaki;
+}
