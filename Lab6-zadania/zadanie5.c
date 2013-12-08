40c0aed3836362f09769af39bda66ee757ad96ee
jkowalska <jkowalska@sigma.ug.edu.pl>
2013-12-08 12:51:17 +0100

    Update zadanie5.c

--- Lab6-zadania/zadanie5.c
+++ Lab6-zadania/zadanie5.c
@@ -1,4 +1,4 @@
-/* Napisz program, który wyświetla prostokąt, owal, strzałkę 
+/* Napisz program, który wyświetla prostokąt, owal, strzałkę
 i diament w/g poniższego wzoru:
 
 *****    **      *      *
@@ -7,7 +7,8 @@ i diament w/g poniższego wzoru:
 *   *  *    *    *    *   *
 *   *   *  *     *     * *
 *****    **      *      *
-Do każdej figury użyj osobnej funkcji, np. funkcja prostokat 
+
+Do każdej figury użyj osobnej funkcji, np. funkcja prostokat
 powinna dać się użyć do rysowania prostokątów różnej wielkości:
 
 void prostokat(int szerokosc, int wysokosc); */
@@ -18,34 +19,56 @@ void owal(int szerokosc, int wysokosc);
 void strzalka(int szerokosc, int wysokosc);
 void diament(int szerokosc, int wysokosc);
 
-main()
-{
+void podstawa(int szerokosc);
+void boki(int szerokosc, int wysokosc);
+
+int main() {
   int szerokosc, wysokosc;
 
-  printf("Podaj szerokosc figur:\n");
+  printf("Podaj szerokosc figur: ");
   scanf("%d", &szerokosc);
-  printf("Podaj wysokosc figur:\n");
+  printf("Podaj wysokosc figur: ");
   scanf("%d", &wysokosc);
+
+  printf("\n");
+
+  prostokat(szerokosc, wysokosc);
 
   return 0;
 }
 
-void prostokat(int szerokosc, int wysokosc)
-{
-  
+void prostokat(int szerokosc, int wysokosc) {
+	podstawa(szerokosc);
+  boki(szerokosc, wysokosc - 2);
+	podstawa(szerokosc);
 }
 
-void owal(int szerokosc, int wysokosc)
-{
+void podstawa(int szerokosc) {
+	int i;
+	for(i = 0; i < szerokosc; i++)
+	  putchar("*");
+	putchar("\n");
+}
+
+void boki(int szerokosc, int wysokosc) {
+	for(int i = 0; i < wysokosc; i++) {
+		putchar("*");
+
+		putchar(" "); putchar(" "); putchar(" "); putchar(" ");		// do wyliczenia
+
+		putchar("*");
+		putchar("\n");
+	}
+}
+
+void owal(int szerokosc, int wysokosc) {
 
 }
 
-void strzalka(int szerokosc, int wysokosc)
-{
+void strzalka(int szerokosc, int wysokosc) {
 
 }
 
-void diament(int szerokosc, int wysokosc)
-{
+void diament(int szerokosc, int wysokosc) {
 
 }
