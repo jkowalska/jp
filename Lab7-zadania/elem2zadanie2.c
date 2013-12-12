51ff942b86f6fc161a38df140dc31c1d8fb4f422
jkowalska <jkowalska@sigma.ug.edu.pl>
2013-12-13 00:52:15 +0100

    dodano zadania

---
+++ Lab7-zadania/elem2zadanie2.c
@@ -0,0 +1,28 @@
+/* Program ma sprawdzać czy podana liczba całkowita jest pierwsza */
+
+#include <stdio.h>
+
+int main() {
+	int n, wynik;
+	  
+  printf("Podaj liczbę całkowitą: ");
+  scanf("&d", &n);
+  
+  wynik = jest_pierwsza(n);
+
+  if (wynik = 1)
+  	printf("Podana liczba '%d' jest pierwsza\n", n);
+  else
+  	printf("Podana liczba '%d' jest złożona\n", n);
+}
+
+int jest_pierwsza(int a) {
+	int x;
+
+	for(x = 2; x <= a - 1; x++) {
+		if (a%x == 0)
+			return 0;
+		else if (x == a)
+			return 1;
+	}
+}
