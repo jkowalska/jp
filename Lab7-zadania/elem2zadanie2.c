0faac6845be318af999f40fb8e7aa99041892bb6
jkowalska <jkowalska@sigma.ug.edu.pl>
2013-12-14 23:58:23 +0100

    Update elem2zadanie2.c

--- Lab7-zadania/elem2zadanie2.c
+++ Lab7-zadania/elem2zadanie2.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 int main() {
-	int n, wynik;
+  int n, wynik;
 	  
   printf("Podaj liczbę całkowitą: ");
   scanf("&d", &n);
@@ -11,18 +11,18 @@ int main() {
   wynik = jest_pierwsza(n);
 
   if (wynik = 1)
-  	printf("Podana liczba '%d' jest pierwsza\n", n);
+    printf("Podana liczba '%d' jest liczbą pierwszą\n", n);
   else
-  	printf("Podana liczba '%d' jest złożona\n", n);
+    printf("Podana liczba '%d' jest liczbą złożoną\n", n);
 }
 
 int jest_pierwsza(int a) {
-	int x;
+  int x;
 
-	for(x = 2; x <= a - 1; x++) {
-		if (a%x == 0)
-			return 0;
-		else if (x == a)
-			return 1;
-	}
+  for(x = 2; x <= a - 1; x++) {
+  if (a % x == 0)
+    return 0;
+  else if (x == a)
+    return 1;
+  }
 }
