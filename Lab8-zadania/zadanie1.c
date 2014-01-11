3d05278b125655a5b1815321a8336eaef4c1ff7a
jkowalska <jkowalska@sigma.ug.edu.pl>
2014-01-12 00:11:06 +0100

    Update zadanie1.c

--- Lab8-zadania/zadanie1.c
+++ Lab8-zadania/zadanie1.c
@@ -1,3 +1,5 @@
+/*Co wypisuje następujący program:*/
+
 #include <stdio.h>
 
 int main () {
@@ -8,3 +10,12 @@ int main () {
   for (indeks = 0, wsk = ref; indeks < 4; indeks++, wsk++)
     printf("%d %d\n", ref[indeks], *wsk);
 }
+
+/* 
+8 8
+4 4
+0 0
+2 2
+*/
+
+/*Ile elementów ma tablica 'ref'? Czego adresem jest 'ref'? a 'ref+1'?*/
