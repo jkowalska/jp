0aa726e015170f79b285f9d40be005818f53e8e8
jkowalska <jkowalska@sigma.ug.edu.pl>
2014-01-18 23:18:55 +0100

    Update elem2zadanie1.c

--- Lab7-zadania/elem2zadanie1.c
+++ Lab7-zadania/elem2zadanie1.c
@@ -1,19 +1,20 @@
+/*Poniższy – zawierający wiele błędów – program dla podanej 
+z klawiatury liczby M ma obliczyć najmniejszą liczbę n, taką że
+1 + 2 + 3 +...+ n >= M
+Popraw wszystkie błędy w tym programie tak, aby poprawiony program 
+prawidłowo obliczał liczbę n. */
+
 #include <stdio.h>
 
 int main() {
-	int M;
-  int suma = 0;
-  int i = 1;
-
-  printf("Podaj liczbę M: ");
-  scanf("%d\n", &M);
-
+  int M, n, suma = 0, i = 1;
+  printf("Podaj liczbe M: ");
+  scanf("%d", &M);
   while (suma < M) {
-     suma=suma+i;
-     i=i+1;
+    suma = suma+i;
+    i++;
   }
-
-  int n=i+1;
+  n = --i;
   printf("Szukane n to: %d\n", n);
   return 0;
 }
